75b7b3d819dca08b01556381623aefd91fab80b9
Thibaut Sacreste <thibaut.sacreste@gmail.com>
2016-04-18 13:56:45 +0100

    req/sec

--- src/index.js
+++ src/index.js
@@ -13,4 +13,6 @@ function messageSource(webSocketUrl) {
 messageSource("ws://127.0.0.1:9090/")
   .pluck('data')
   .map(JSON.parse)
+  .batchWithTimeOrCount(1000, Number.MAX_SAFE_INTEGER)
+  .pluck('length')
   .each(x => console.log(x))
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+var production = process.env.NODE_ENV === 'production';
+
 module.exports = {
+    debug:   !production,
+    devtool: production ? false : 'eval',
     entry:  './src',
     output: {
         path:     'builds',
