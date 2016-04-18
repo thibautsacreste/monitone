590fde92fc76d5bac9a22711275f8b3efae8b77e
Thibaut Sacreste <thibaut.sacreste@gmail.com>
2016-04-18 14:09:23 +0100

    forking streams and computes avg response time

--- src/index.js
+++ src/index.js
@@ -10,9 +10,25 @@ function messageSource(webSocketUrl) {
   });
 };
 
-messageSource("ws://127.0.0.1:9090/")
+const dataStream = messageSource("ws://127.0.0.1:9090/")
   .pluck('data')
   .map(JSON.parse)
+
+const debugStream = dataStream.fork();
+debugStream
+  .throttle(1000)
+  .each(x => console.log(x))
+
+const reqPerSecStream = dataStream.fork();
+reqPerSecStream
   .batchWithTimeOrCount(1000, Number.MAX_SAFE_INTEGER)
   .pluck('length')
-  .each(x => console.log(x))
+  .each(x => console.log(`req/sec: ${x}`))
+
+const avgRespTimeStream = dataStream.fork();
+avgRespTimeStream
+  .pluck('request_time')
+  .map(t => 1000 * parseFloat(t))
+  .batchWithTimeOrCount(1000, Number.MAX_SAFE_INTEGER)
+  .map(arr => arr.reduce((a, b) => a + b) / arr.length)
+  .each(x => console.log(`response time:${x}`))
