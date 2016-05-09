b32ddb4142d1529ccc984b9638792e4bafd2c011
Thibaut Sacreste <thibaut.sacreste@gmail.com>
2016-05-09 16:54:41 +0100

    Moving multicast experiment to the dumping ground

---
+++ src/dump.js
@@ -0,0 +1,43 @@
+// Ideas dump
+import _ from 'highland';
+
+function multicast (children, source) {
+  return function(source) {
+    const forks = children.map(c => source.fork());
+    children.forEach((c, i) => c(forks[i]));
+  }
+}
+
+messageSource("ws://127.0.0.1:9090/")
+  .pluck('data')
+  .map(JSON.parse)
+  .through(
+    multicast([
+      s => {
+        s.through(reqPerSec)
+         .each(x => console.log(`req/sec: ${x}`))
+      },
+      s => {
+        s.through(avgRespTime)
+         .each(x => console.log(`response time: ${x}`))
+      }
+    ])
+  );
+
+function dummySource() {
+  let count = 0;
+  return _(function (push, next) {
+    const pushIt = () => {
+      push(null, count+=1);
+      setTimeout(pushIt, 3000);
+    }
+    pushIt();
+  });
+};
+
+dummySource()
+  .through(timeWindow(1000))
+  .batchWithTimeOrCount(1000, Number.MAX_SAFE_INTEGER)
+  .pluck('length')
+  .each(_.log)
+  .each(x => console.log(`req/sec: ${x}`));
--- src/index.js
+++ src/index.js
@@ -10,24 +10,6 @@ function messageSource(webSocketUrl) {
   });
 };
 
-function dummySource() {
-  let count = 0;
-  return _(function (push, next) {
-    const pushIt = () => {
-      push(null, count+=1);
-      setTimeout(pushIt, 3000);
-    }
-    pushIt();
-  });
-};
-
-function multicast (children, source) {
-  return function(source) {
-    const forks = children.map(c => source.fork());
-    children.forEach((c, i) => c(forks[i]));
-  }
-}
-
 const timeWindow = _.curry(function(ms, source) {
   let batched = [],
       ended = false,
@@ -70,30 +52,19 @@ const avgRespTime = _.seq(
   _.pluck('request_time'),
   _.map(t => 1000 * parseFloat(t)),
   timeWindow(1000),
-  _.map(arr => arr.length? arr.reduce((a, b) => a + b) / arr.length : 0)
+  _.map(a => a.length? a.reduce((x, y) => x + y) / a.length : 0)
 );
 
-// dummySource()
-  // .through(timeWindow(1000))
-  // .batchWithTimeOrCount(1000, Number.MAX_SAFE_INTEGER)
-  // .pluck('length')
-  // .each(_.log)
-  // .each(x => console.log(`req/sec: ${x}`));
-
-messageSource("ws://127.0.0.1:9090/")
+const requests = messageSource("ws://127.0.0.1:9090/")
   .pluck('data')
-  .map(JSON.parse)
-  // .through(reqPerSec)
-  // .each(x => console.log(`req/sec: ${x}`))
-  .through(
-    multicast([
-      s => {
-        s.through(reqPerSec)
-         .each(x => console.log(`req/sec: ${x}`))
-      },
-      s => {
-        s.through(avgRespTime)
-         .each(x => console.log(`response time: ${x}`))
-      }
-    ])
-  );
+  .map(JSON.parse);
+
+requests
+  .fork()
+  .through(reqPerSec)
+  .each(x => console.log(`req/sec: ${x}`));
+
+requests
+  .fork()
+  .through(avgRespTime)
+  .each(x => console.log(`response time: ${x}`));
