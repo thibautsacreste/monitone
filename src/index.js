aca981650f5094681103cb67b0e472050ca98c03
Thibaut Sacreste <thibaut.sacreste@gmail.com>
2016-05-09 16:21:24 +0100

    timeWindow function that always fires at interval even if hasn't received any events

--- src/index.js
+++ src/index.js
@@ -10,6 +10,17 @@ function messageSource(webSocketUrl) {
   });
 };
 
+function dummySource() {
+  let count = 0;
+  return _(function (push, next) {
+    const pushIt = () => {
+      push(null, count+=1);
+      setTimeout(pushIt, 3000);
+    }
+    pushIt();
+  });
+};
+
 function multicast (children, source) {
   return function(source) {
     const forks = children.map(c => source.fork());
@@ -17,8 +28,41 @@ function multicast (children, source) {
   }
 }
 
+const timeWindow = _.curry(function(ms, source) {
+  let batched = [],
+      ended = false,
+      interval,
+      pushFn = () => {};
+
+  const flush = () => {
+    pushFn(null, batched);
+    batched = [];
+    if (ended) {
+      pushFn(null, nil);
+      clearInterval(interval);
+    }
+  }
+
+  interval = setInterval(flush, ms);
+
+  return source.consume(function (err, x, push, next) {
+    pushFn = push;
+    if (err) {
+      push(err);
+      next();
+    }
+    else if (x === nil) {
+      ended = true;
+    }
+    else {
+      batched.push(x);
+      next();
+    }
+  });
+});
+
 const reqPerSec = _.seq(
-  _.batchWithTimeOrCount(1000, Number.MAX_SAFE_INTEGER),
+  timeWindow(1000),
   _.pluck('length')
 );
 
@@ -29,18 +73,27 @@ const avgRespTime = _.seq(
   _.map(arr => arr.reduce((a, b) => a + b) / arr.length)
 );
 
+dummySource()
+  // .through(timeWindow(1000))
+  // .batchWithTimeOrCount(1000, Number.MAX_SAFE_INTEGER)
+  // .pluck('length')
+  // .each(_.log)
+  // .each(x => console.log(`req/sec: ${x}`));
+
 messageSource("ws://127.0.0.1:9090/")
   .pluck('data')
   .map(JSON.parse)
-  .through(
-    multicast([
-      s => {
-        s.through(reqPerSec)
-         .each(x => console.log(`req/sec: ${x}`))
-      },
-      s => {
-        s.through(avgRespTime)
-         .each(x => console.log(`response time: ${x}`))
-      }
-    ])
-  );
+  .through(reqPerSec)
+  .each(x => console.log(`req/sec: ${x}`))
+  // .through(
+  //   multicast([
+  //     s => {
+  //       s.through(reqPerSec)
+  //        .each(x => console.log(`req/sec: ${x}`))
+  //     },
+  //     s => {
+  //       s.through(avgRespTime)
+  //        .each(x => console.log(`response time: ${x}`))
+  //     }
+  //   ])
+  // );
